30db63ee9bd1c7cfc6c7383c911a2259c5b732d6
pascal <okeziepascal2@gmail.com>
2024-09-22 09:45:16 +0100

    set up

--- server.js
+++ server.js
@@ -5,22 +5,37 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
+
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"});
+});
 
 /* ***********************
  * Log statement to confirm server operation
